5f602cc0ac5d5254df81b0517303a4f87e91f812
HellenWeb <84445465+HellenWeb@users.noreply.github.com>
2021-09-08 19:09:05 +1000

    Add new file and changed them

---
+++ bot.py
@@ -0,0 +1,22 @@
+
+# Modules
+
+import config
+import telebot
+from telebot import types
+
+# Default Variebles
+
+bot = telebot.TeleBot(config.token)
+
+# Logic Bot
+
+@bot.message_handler(content_types=['text'])
+def send_you_message(message):
+    bot.send_message(message.chat.id, message.text)
+
+if __name__ == '__main__':
+    bot.infinity_polling()
+
+
+    
